4151e8e5f3a0240bc39c62c39942d95efa41df5b
ChipLag <142794877+ChipLag@users.noreply.github.com>
2023-09-01 17:08:08 +0200

    Fix R6

--- universal/simpleinfjump.lua
+++ universal/simpleinfjump.lua
@@ -4,9 +4,9 @@ local p = Instance.new("Part")
 p.Parent = game.Workspace
 p.Anchored = true
 p.Transparency = 1
-local anchor = "LeftFoot"
+local anchor = "HumanoidRootPart"
 while true do
   p.Position = game.Players.LocalPlayer.Character:FindFirstChild(anchor).Position
-    p.Position = p.Position + Vector3.new(0,-3,0)
+    p.Position = p.Position + Vector3.new(0,-6,0)
       wait(0.1)
       end
