72adaaf330caf5d2ef44ce45abee7d334ab3100f
ChipLag <142794877+ChipLag@users.noreply.github.com>
2026-06-05 14:10:27 +0200

    Create hubv2.lua

---
+++ hubv2.lua
@@ -0,0 +1,17 @@
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/ChipLag/rb/main/hubv2.lua",true))() 
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+
+local gui = Library:create( Theme = Library.Themes.Serika )
+
+local tabUniversal = gui:tab{
+    Name = "Universal"
+}
+
+tabUniversal:button({
+    Name = "Nameless Admin",
+    Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"))() 
+    end,
+})
+
